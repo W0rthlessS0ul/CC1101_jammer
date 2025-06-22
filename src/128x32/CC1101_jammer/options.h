48048865426d65e0a4a966f32b58c7089294656c
W0rthlessS0ul <176677387+W0rthlessS0ul@users.noreply.github.com>
2025-06-22 08:46:26 +0300

    new version
    Added support for 128x32 display
    Added support for without oled version

---
+++ src/128x32/CC1101_jammer/options.h
@@ -0,0 +1,30 @@
+//changeable parameters
+float keyfob_list[] = {303.00, 310.00, 315.00, 330.00, 350.00, 370.00, 390.00, 418.00, 433.92};
+const char *ssid = "jammer";
+const char *password = "W0rthlessS0ul";
+//better not change
+GButton buttNEXT(14);
+GButton buttOK(12);
+GButton buttPREVIOUS(13);
+byte sck = 18;
+byte miso = 19;
+byte mosi = 23;
+byte ss = 5;
+int gdo0 = 2;
+int gdo2 = 4;
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define EEPROM_SIZE 512
+//unchangeable parameters
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+WebServer server(80); 
+byte* jamdata;
+int menu_number = 0;
+int logo;
+int access_point;
+int jam_delay;
+float range_step;
+bool jam_break = false;
+byte payload;
